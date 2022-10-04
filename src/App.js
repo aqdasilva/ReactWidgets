5e6250c04cb5de04b88a90cae62b2f4048892e0e
aqdasilva <antqdasilva@gmail.com>
2022-10-04 13:23:37 -0500

    added ctrKey, tabs for each app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React, { useState } from "react";
+import React, { Component, useState } from "react";
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
 import Translate from "./components/Translate";
+import Route from "./components/Route";
+import Header from "./components/Header";
+
 
 const items = [
     {
@@ -35,37 +38,35 @@ const options = [
     }
 ];
 
-const showAccordion = () => {
-    if (window.location.pathname === '/accordion') {
-        return <Accordion items={items} />;
-    }
-};
-
-const showList = () => {
-    if (window.location.pathname === '/list') {
-        return <Search />;
-    }
-};
-
-const showDropdown = () => {
-    if (window.location.pathname === '/dropdown' ) {
-        return <Dropdown />;
-    }
-};
-
-const showTranslate = () => {
-    if (window.location.pathname === '/translate') {
-        return <Translate />;
-    }
+const showComponent = (route, Component) => {
+    return window.location.pathname === route
+        ? Component
+        : null;
 };
 
 export default () => {
+    const [selected, setSelected] = useState(options[0]);
+
     return (
         <div>
-            {showAccordion()}
-            {showList()}
-            {showDropdown()}
-            {showTranslate()}
+            <Header></Header>
+            <Route path="/">
+                <Accordion items={items} />
+            </Route>
+            <Route path={"/list"}>
+                <Search />
+            </Route>
+            <Route path={"/dropdown"}>
+                <Dropdown
+                    label="select a color"
+                    options={options}
+                    selected={selected}
+                    onSelectedChange={setSelected}
+                     />
+            </Route>
+            <Route path={"/translate"}>
+                <Translate />
+            </Route>
         </div>
     );
 };
