026ceb21cf3639dff9a8160412848ed6574598a5
aqdasilva <antqdasilva@gmail.com>
2022-10-04 09:31:38 -0500

    added toogle dropdown

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,12 +1,25 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
+
 
 const Dropdown = ( { options, selected, onSelectedChange }) => {
     const [open, setOpen] = useState(false);
+    const ref = useRef();
 
     useEffect(() => {
-        document.body.addEventListener('click', () => {
+        const onBodyClick = (event) => {
+            if ( ref.current.contains(event.target)) {
+                return;
+            }
+            
             setOpen(false);
-        }, {capture: true});
+        };
+
+        document.body.addEventListener('click', onBodyClick);
+
+        return () => {
+            document.body.removeEventListener('click', onBodyClick);
+        };
+            
     }, []);
 
     const renderedOptions = options.map((option) => {
@@ -24,8 +37,10 @@ const Dropdown = ( { options, selected, onSelectedChange }) => {
         );
     });
 
+
+
     return (
-        <div className="ui form">
+        <div  ref={ref} className="ui form">
             <div className="field">
                 <label className="label">Select your favorite color</label>
                 <div onClick={() => setOpen(!open)} 
